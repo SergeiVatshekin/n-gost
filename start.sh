b2778d3f565f7d88ade25e9724ab1a99c0598669
Sergei Vatshekin <sergei.vatshekin@yandex.ru>
2020-11-18 12:01:04 +0300

    current

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 cd /cprocsp-install/linux-amd64_deb && dpkg -i ./lsb-cprocsp-kc2-*
 
-/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\esiadevtls' -provtype 81 -provname "Crypto-Pro GOST R 34.10-2012 KC2 Strong CSP"
+/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\esiadev' -provtype 81 -provname "Crypto-Pro GOST R 34.10-2012 KC2 Strong CSP"
 /opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=esia-dev-k8s.test.gosuslugi.ru" -dest /etc/ssl/certs/srvtlsGost.cer
 openssl x509 -inform DER -in /etc/ssl/certs/srvtlsGost.cer -out /etc/ssl/certs/srvtlsGost.pem
 
